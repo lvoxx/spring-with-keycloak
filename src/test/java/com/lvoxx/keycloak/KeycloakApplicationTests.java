c9a76009d73ced6d5cf1693dd0ae75647a108b51
lvoxx <lvoxxartist@gmail.com>
2024-04-18 14:36:08 +0700

    First push init project

---
+++ src/test/java/com/lvoxx/keycloak/KeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvoxx.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
